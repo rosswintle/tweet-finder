831260e8cd33aed790e2dd72359a4afcee875a75
Ross Wintle <rosswintle@users.noreply.github.com>
2022-03-02 16:04:32 +0000

    Update to Tailwind v3 + CLI and add and ad

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    purge: [
+    content: [
         './*.html',
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [
